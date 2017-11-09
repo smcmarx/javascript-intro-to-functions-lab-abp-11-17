5b75fda934db47c6d8f9c792f36e4043f1a8e9cf
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-09 19:45:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string) === 'hello' {
     console.log("I can't hear you!")
-  } else if string == .toUpperCase {
+  } else if string === .toUpperCase {
     console.log("Yes INDEED!")
-  } else if string == 'I love you, Grandma.' {
+  } else if string === 'I love you, Grandma.' {
     console.log("I love you, too.")
   }
 
