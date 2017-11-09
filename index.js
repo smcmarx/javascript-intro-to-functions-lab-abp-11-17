d0de4227c76d5f13473e30f96f0ae8abe282dd27
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-09 19:57:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
 
-  if (string.toLowerCase() === string) {
+  if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
 
